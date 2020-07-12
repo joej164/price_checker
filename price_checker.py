2190e067c1b49d304950bb9943556173b606fcb7
Joe Jacobs <joej@ignw.io>
2020-07-12 06:35:27 -0700

    adding apple store checks

--- price_checker.py
+++ price_checker.py
@@ -11,6 +11,14 @@ def find_playstation_price(url):
     return price
 
 
+def find_apple_store_price(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    price = soup.find_all(
+        class_='inline-list__item inline-list__item--bulleted app-header__list__item--price')[0].get_text()
+    return price
+
+
 class TestClass:
     def setup(self):
         pass
@@ -18,20 +26,26 @@ class TestClass:
     def teardown(self):
         time.sleep(5)   # Sleep for 1 second
 
-    def test_saga_scarlet_grace_price(self):
+    def test_ps_saga_scarlet_grace_price(self):
         url = 'https://store.playstation.com/en-us/product/UP0082-CUSA09653_00-SAGASCARLETHD001'
         expected_price = "$29.99"
         actual_price = find_playstation_price(url)
         assert expected_price == actual_price
 
-    def test_castle_crashers_remastered(self):
+    def test_ps_castle_crashers_remastered(self):
         url = 'https://store.playstation.com/en-us/product/UP2015-CUSA14409_00-CASTLECRASHERSNA'
         expected_price = "$14.99"
         actual_price = find_playstation_price(url)
         assert expected_price == actual_price
 
-    def test_dungeons_3_complete_collection(self):
+    def test_ps_dungeons_3_complete_collection(self):
         url = 'https://store.playstation.com/en-us/product/UP2060-CUSA07824_00-0331006500115282'
         expected_price = "$39.99"
         actual_price = find_playstation_price(url)
+        assert expected_price == actual_price
+
+    def test_apple_saga_scarlet_grace_price(self):
+        url = 'https://apps.apple.com/us/app/saga-scarlet-grace-ambitions/id1237502412'
+        expected_price = "$29.99"
+        actual_price = find_apple_store_price(url)
         assert expected_price == actual_price
