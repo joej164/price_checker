e581bd11b32b51e43d5be2eac2e1d438ddd7f2c0
Joe Jacobs <joej@ignw.io>
2020-07-12 06:17:22 -0700

    updating price

--- price_checker.py
+++ price_checker.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pytest
+import time
 
 
 def find_playstation_price(url):
@@ -10,8 +11,27 @@ def find_playstation_price(url):
     return price
 
 
-def test_saga_scarlet_grace_price():
-    url = 'https://store.playstation.com/en-us/product/UP0082-CUSA09653_00-SAGASCARLETHD001'
-    expected_price = "$29.99"
-    actual_price = find_playstation_price(url)
-    assert expected_price == actual_price
+class TestClass:
+    def setup(self):
+        pass
+
+    def teardown(self):
+        time.sleep(5)   # Sleep for 1 second
+
+    def test_saga_scarlet_grace_price(self):
+        url = 'https://store.playstation.com/en-us/product/UP0082-CUSA09653_00-SAGASCARLETHD001'
+        expected_price = "$29.99"
+        actual_price = find_playstation_price(url)
+        assert expected_price == actual_price
+
+    def test_castle_crashers_remastered(self):
+        url = 'https://store.playstation.com/en-us/product/UP2015-CUSA14409_00-CASTLECRASHERSNA'
+        expected_price = "$14.99"
+        actual_price = find_playstation_price(url)
+        assert expected_price == actual_price
+
+    def test_dungeons_3_complete_collection(self):
+        url = 'https://store.playstation.com/en-us/product/UP2060-CUSA07824_00-0331006500115282'
+        expected_price = "$39.98"
+        actual_price = find_playstation_price(url)
+        assert expected_price == actual_price
