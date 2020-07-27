8d964395e502004e12a738a762ac6010aaef30ee
Joe Jacobs <ignw2@up.edu>
2020-07-27 08:06:44 -0700

    more debugging

--- conftest.py
+++ conftest.py
@@ -7,15 +7,15 @@ def pytest_runtest_makereport(item, call):
     # execute all other hooks to obtain the report object
     outcome = yield
     rep = outcome.get_result()
-    print('$$$$$$$$$$$$$$$$$$$$$$')
     # we only look at actual failing test calls, not setup/teardown
     if rep.when == "call" and rep.failed:
-        mode = "a" if os.path.exists("failures") else "w"
+        mode = "a" if os.path.exists("failures.txt") else "w"
         with open("failures.txt", mode) as f:
             # let's also access a fixture for the fun of it
             if "tmpdir" in item.fixturenames:
+                print('in tmpdir')
                 extra = " ({})".format(item.funcargs["tmpdir"])
             else:
                 extra = ""
-
+            print(rep.nodeid, extra)
             f.write(rep.nodeid + extra + "\n")
