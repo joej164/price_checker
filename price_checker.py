294d936cd38ba57044edc3e2d1f8e3150d1d62fa
Joe Jacobs <joej164@gmail.com>
2021-02-13 08:32:03 -0800

    fixing pioneer extraction

--- price_checker.py
+++ price_checker.py
@@ -22,14 +22,11 @@ def find_apple_store_price(url):
 def find_pioneer_firmware_versions(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    all_rows = soup.find_all('tr')
-    my_data = [row.text for row in all_rows if row.attrs.get(
-        'data-model') == 'DMH-W4600NEX']
+    price_str = soup.find(class_='version').text
+    price_arr = price_str.split()
+    price = price_arr[1]
 
-    output_list = []
-    for row in my_data:
-        output_list.extend(row.split())
-    return output_list
+    return price
 
 
 def find_wacom_price(url):
@@ -57,7 +54,7 @@ class TestPlaystation(Common):
 
 class TestPioneer(Common):
     def test_pioneer_software_download_updated_firmware(self):
-        url = 'https://www.pioneerelectronics.com/PUSA/Support/Downloads'
+        url = 'https://www.pioneerelectronics.com/PUSA/Car/NEX/DMH-W4600NEX'
         expected_firmware = '1.31'
         dmh_4600_next_fw = find_pioneer_firmware_versions(url)
         assert expected_firmware in dmh_4600_next_fw
